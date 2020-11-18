65134edffd63991db4a692329f2877bd4f3baf95
Sergi Lange-Soler <sergi.lange123@gmail.com>
2020-11-18 14:59:48 +0000

    added explanation and userStrategy.py

--- naughts-and-crosses.py
+++ naughts-and-crosses.py
@@ -1,16 +1,26 @@
 import random
 
 
-# strategies return a tuple (x, y) of the x and y coordinates they want to place their mark in
+"""
+Board format:
+
+board[0][0] 0, 0 | board[0][1] 1, 0 | board[0][2] 2, 0
+-----------------|------------------|-----------------
+board[1][0] 0, 1 | board[1][1] 1, 1 | board[1][2] 2, 1
+-----------------|------------------|-----------------
+board[2][0] 0, 2 | board[2][1] 1, 2 | board[2][2] 2, 2
+"""
 
 
 def naughtsStrategy(board):
     MY_PIECE = "O"
-    OTHER_PIECE = "X"
+    OTHER_PIECE = "X"  # Refer to marks on the board using these constants
     EMPTY = " "
 
-    if board[0][1] == OTHER_PIECE:
-        return 0, 2
+    if board[0][1] == EMPTY:
+        return 1, 0     # Notice the result is in format `x, y` whereas to access squares on the board you use
+    else:               # `board[y][x]` (where x and y increase downwards and to the right). See above for more detail
+        return 2, 2
 
 
 def crossesStrategy(board):
@@ -18,8 +28,19 @@ def crossesStrategy(board):
     OTHER_PIECE = "O"
     EMPTY = " "
 
-    return 2, 0
+    x = random.randint(0, 2)
+    y = random.randint(0, 2)
+    if board[y][x] == EMPTY:
+        return x, y
+    
+    elif board[y][0] == MY_PIECE:
+        return 1, y
 
+    return 0, 0  # The code must always return a value in all code paths; if it doesn't return a value or if the value is an illegal move, you lose!
+
+
+# This code can be found on https://github.com/Rolodophone/naughts-and-crosses. If you want to test your strategy,
+# replace one of the above strategies with yours and the other with the one in userStrategy.py
 
 def moveIsLegal(move):
     return \
@@ -57,15 +78,17 @@ def thereIsTie():
 
 
 def printBoard():
-    for row in gameBoard:
-        for square in row:
-            print("[{}]".format(square), end="")
-        print()
+    for y, row in enumerate(gameBoard):
+
+        print(" {} | {} | {}".format(row[0], row[1], row[2]))
+
+        if y != 2:
+            print("---|---|---")
 
 
 gameBoard = [
     [" ", " ", " "],
-    [" ", " ", " "], # gameBoard[2][0]
+    [" ", " ", " "],
     [" ", " ", " "]
 ]
 
@@ -73,6 +96,13 @@ currentStrategy = naughtsStrategy
 
 while True:
     nextMove = currentStrategy(gameBoard)
+
+    if nextMove is None:
+        if currentStrategy == naughtsStrategy:
+            result = "Naughts did not return a move so crosses wins by default"
+        else:
+            result = "Crosses did not return a move so naughts wins by default"
+        break
 
     if moveIsLegal(nextMove):
         # perform move
---
+++ userStrategy.py
@@ -0,0 +1,3 @@
+def userStrategy(board):
+    coords = input("Please enter your move in the format \"x y\": ")
+    return int(coords[0]), int(coords[2])
